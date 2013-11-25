b107e9275c5196217dd49aac159cb943c08a770f
Ryan <rszemplinski22@gmail.com>
2013-11-25 19:22:17 +0000

    Removed Help and added Threads

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131121044545) do
+ActiveRecord::Schema.define(version: 20131125192055) do
+
+  create_table "forums", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email"
