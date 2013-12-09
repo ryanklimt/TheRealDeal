e381ee9b9f2ae58e006cd3fbfc40285d367e9bb8
Ryan Klimt <ryan@skagga.com>
2013-12-09 18:43:44 -0500

    Fixes

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,4 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 UnknownBusiness::Application.initialize!
 
-ActionMailer::Base.smtp_settings = { :address => "smtp.gmail.com", :port => 465, :user_name => "ryanklimt@gmail.com", :password => "ryanjohn", :authentication => :login}
+ActionMailer::Base.smtp_settings = { :address => "smtp.gmail.com", :port => 465, :user_name => "ryanklimt@gmail.com", :password => "Ryanjohn64", :authentication => :login}
