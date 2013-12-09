26e7e6884128cd53d909e51b32584f5a769c47e5
Ryan Klimt <ryan@skagga.com>
2013-12-09 18:49:36 -0500

    Config mailer

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,12 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 UnknownBusiness::Application.initialize!
 
-ActionMailer::Base.smtp_settings = { :address => "smtp.gmail.com", :port => 465, :user_name => "ryanklimt@gmail.com", :password => "Ryanjohn64", :authentication => :login}
+ActionMailer::Base.smtp_settings = {
+  :user_name => "app20109224@heroku.com",
+  :password => "tj23ktel",
+  :domain => "ultimatebeta.com",
+  :address => "smtp.sendgrid.net", 
+  :port => 586,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
