8dde0782ddbb40b80101e8003f8c6353ff199fbd
Ryan Klimt <ryan@skagga.com>
2013-12-09 18:36:19 -0500

    Stmp mail

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 UnknownBusiness::Application.initialize!
+
+config.action_mailer.delivery_method = :smtp
+
+ActionMailer::Base.smtp_settings = { :address => "smtp.gmail.com", :port => 465, :user_name => "ryanklimt@gmail.com", :password => "ryanjohn", :authentication => :login}
