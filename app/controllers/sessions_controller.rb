230c860a9c417f19635f73568dbc6a3dcf8b879b
Ryan <rszemplinski22@gmail.com>
2013-12-08 21:22:36 -0500

    added a remember me thing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,11 @@ class SessionsController < ApplicationController
     # Checks email first and if nothing checks username
     @user ||= User.first(:conditions => [ "lower(username) = ?", params[:session][:email_username].downcase]) unless @user = User.find_by(email: params[:session][:email_username].downcase)
     if @user and @user.authenticate(params[:session][:password])
-      sign_in @user
+      if params[:session][:remember_me] == '1' then
+        sign_in(@user, true)
+      else
+        sign_in(@user, false)
+      end
       flash[:success] = "Welcome " + @user.username + "!"
       redirect_to root_path
     else
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,10 +1,17 @@
 module SessionsHelper
   
-  def sign_in(user)
-    remember_token = User.new_remember_token
-    cookies.permanent[:remember_token] = remember_token
-    user.update_attribute(:remember_token, User.encrypt(remember_token))
-    self.current_user = user
+  def sign_in(user, remember)
+    if remember then
+      remember_token = User.new_remember_token
+      cookies.permanent[:remember_token] = remember_token
+      user.update_attribute(:remember_token, User.encrypt(remember_token))
+      self.current_user = user
+    else
+      remember_token = User.new_remember_token
+      cookies[:remember_token] = { value: remember_token, expires: 8.hours.from_now }
+      user.update_attribute(:remember_token, User.encrypt(remember_token))
+      self.current_user = user
+    end
   end
   
   def signed_in?
