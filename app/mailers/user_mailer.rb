e00f55f97390112a08afd122b70e742b350357b0
Ryan Klimt <ryan@skagga.com>
2013-12-09 19:26:35 -0500

    Fix

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default from: "rszemplinski22@gmail.com"
+  default from: "mailer@ultimatebeta.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
