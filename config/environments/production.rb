f1f70bb8a2f63f0a7c51f7ded268511b06af11a4
Ryan Klimt <ryan@skagga.com>
2013-12-09 19:19:31 -0500

    Fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ UnknownBusiness::Application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = {
+  config.action_mailer.smtp_settings = {
           :address        => 'smtp.sendgrid.net',
           :port           => '587',
           :authentication => :plain,
