e12f67044008e487f7d73ee2db5949ae40590c7b
Ryan Klimt <ryan@skagga.com>
2013-12-09 19:04:15 -0500

    Fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,6 @@ UnknownBusiness::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-
+  
+  config.action_mailer.default_url_options = { :host => 'ultimatebeta.com' } 
 end
