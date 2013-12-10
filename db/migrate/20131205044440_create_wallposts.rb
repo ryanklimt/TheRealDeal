1d3db978efa099301624c97f56392465dde0f783
Ryan Klimt <ryan@skagga.com>
2013-12-09 21:00:32 -0500

    Fixup of db and added directed_id

--- db/migrate/20131205044440_create_wallposts.rb
+++ db/migrate/20131205044440_create_wallposts.rb
@@ -3,7 +3,7 @@ class CreateWallposts < ActiveRecord::Migration
     create_table :wallposts do |t|
       t.string :content
       t.integer :user_id
-      t.integer :direted_id
+      t.integer :direted_user_id
       t.timestamps
     end
     add_index :wallposts, [:user_id, :created_at]
