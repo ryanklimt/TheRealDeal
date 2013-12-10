922efff7ddec38a747c596234add0a8848233f97
Ryan Klimt <ryan@skagga.com>
2013-12-09 19:34:55 -0500

    Fix

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default from: "app20109224@heroku.com"
+  #default from: "ryanklimt@gmail.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
