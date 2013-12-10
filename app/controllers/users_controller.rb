97b0af2aadbce6c93d9fe1e40f20cc129b8ebae7
Ryan <rszemplinski22@gmail.com>
2013-12-09 23:49:32 -0500

    Removed gender from user new page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -100,7 +100,7 @@ class UsersController < ApplicationController
   private
   
     def user_params
-      params.require(:user).permit(:username, :email, :gender, :password, :password_confirmation)
+      params.require(:user).permit(:username, :email, :password, :password_confirmation)
     end
     
     def update_params
