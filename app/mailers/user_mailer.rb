3f916a7eea640d7942bee96a237b75e019828cdf
Ryan Klimt <ryan@skagga.com>
2013-12-09 20:10:52 -0500

    Sexy fixes

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  #default from: "ryanklimt@gmail.com"
+  default from: "no-reply@ultimatebeta.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
