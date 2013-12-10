42ac39e77255c81cd95865a0d6e516dad677b449
Ryan Klimt <ryan@skagga.com>
2013-12-09 19:56:13 -0500

    Fix

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 UnknownBusiness::Application.initialize!
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :user_name => "app20109224@heroku.com",
+  :password => "tj23ktel",
+  :domain => "heroku.om",
+  :address => "smtp.sendgrid.net",
+  :port => '587',
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
