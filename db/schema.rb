7072a4fba6f7c08758e949a551d2b1d5bb647622
Ryan <rszemplinski22@gmail.com>
2013-12-14 16:09:13 -0500

    Stuffs

--- db/schema.rb
+++ db/schema.rb
@@ -56,8 +56,6 @@ ActiveRecord::Schema.define(version: 20131213212428) do
     t.datetime "updated_at"
   end
 
-  add_index "subscriptions", ["subforum_id", "user_id"], name: "index_subscriptions_on_subforum_id_and_user_id", unique: true
-
   create_table "topics", force: true do |t|
     t.string   "title"
     t.integer  "last_poster_id"
